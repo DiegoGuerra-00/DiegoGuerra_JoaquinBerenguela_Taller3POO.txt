e3d857b722411c35692833feec9b80e6c2cb1d55
DiegoGuerra-00 <diego.guerra01@alumnos.ucn.cl>
2025-11-21 22:41:05 -0300

    Update SistemaImpl.java

--- Taller3/src/taller3/SistemaImpl.java
+++ Taller3/src/taller3/SistemaImpl.java
@@ -1,3 +1,8 @@
+
+// Diego Benjamin Guerra Estay 21686594-4 ICCI
+// Joaquin Antonio Berenguela Caimanque 21708988-3  
+
+
 package taller3;
 
 import java.io.FileWriter;
@@ -368,3 +373,4 @@ public class SistemaImpl implements Sistema {
 	
 
 }
+
