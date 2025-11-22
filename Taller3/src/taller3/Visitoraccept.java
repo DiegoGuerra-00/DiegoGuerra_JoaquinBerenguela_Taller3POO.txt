0ef94bc156441da9979b19a0414744c44049569a
DiegoGuerra-00 <diego.guerra01@alumnos.ucn.cl>
2025-11-21 22:42:36 -0300

    Update Visitoraccept.java

--- Taller3/src/taller3/Visitoraccept.java
+++ Taller3/src/taller3/Visitoraccept.java
@@ -1,6 +1,12 @@
+// Diego Benjamin Guerra Estay 21686594-4 ICCI
+// Joaquin Antonio Berenguela Caimanque 21708988-3  
+
+
+
 package taller3;
 
 public interface Visitoraccept {
 	void accept(Visitor visitor);
 
 }
+
