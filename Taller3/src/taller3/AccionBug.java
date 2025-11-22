ab3e9328ccd6df06b8f04d6640927c925e50ad46
DiegoGuerra-00 <diego.guerra01@alumnos.ucn.cl>
2025-11-21 22:36:14 -0300

    menul listo

--- Taller3/src/taller3/AccionBug.java
+++ Taller3/src/taller3/AccionBug.java
@@ -1,3 +1,8 @@
+// Diego Benjamin Guerra Estay 21686594-4 ICCI
+// Joaquin Antonio Berenguela Caimanque 21708988-3  
+
+
+
 package taller3;
 
 import java.util.ArrayList;
@@ -17,3 +22,4 @@ public class AccionBug implements Visitor{
 	}
 
 }
+
