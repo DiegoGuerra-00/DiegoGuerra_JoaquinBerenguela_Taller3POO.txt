00b54c33bc985f63f1be704663eaa1afb9c94bf4
DiegoGuerra-00 <diego.guerra01@alumnos.ucn.cl>
2025-11-21 22:37:21 -0300

    Update Factory.java

--- Taller3/src/taller3/Factory.java
+++ Taller3/src/taller3/Factory.java
@@ -1,3 +1,7 @@
+// Diego Benjamin Guerra Estay 21686594-4 ICCI
+// Joaquin Antonio Berenguela Caimanque 21708988-3  
+
+
 package taller3;
 
 public interface Factory {
@@ -8,3 +12,4 @@ public interface Factory {
 	
 
 }
+
