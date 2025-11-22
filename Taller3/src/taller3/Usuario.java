1dd300afca881acca18d0617584203e4276bd0a4
DiegoGuerra-00 <diego.guerra01@alumnos.ucn.cl>
2025-11-21 22:41:56 -0300

    Update Usuario.java

--- Taller3/src/taller3/Usuario.java
+++ Taller3/src/taller3/Usuario.java
@@ -1,3 +1,8 @@
+// Diego Benjamin Guerra Estay 21686594-4 ICCI
+// Joaquin Antonio Berenguela Caimanque 21708988-3  
+
+
+
 package taller3;
 
 public class Usuario {
@@ -33,3 +38,4 @@ public class Usuario {
 	
    //-
 }
+
