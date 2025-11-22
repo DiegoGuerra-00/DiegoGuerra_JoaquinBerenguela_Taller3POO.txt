5e0374d35b5397997a68a78b3fe317119f8e119e
DiegoGuerra-00 <diego.guerra01@alumnos.ucn.cl>
2025-11-21 22:38:18 -0300

    Update PrioridadPorComplejidadStrategy.java

--- Taller3/src/taller3/PrioridadPorComplejidadStrategy.java
+++ Taller3/src/taller3/PrioridadPorComplejidadStrategy.java
@@ -1,3 +1,8 @@
+// Diego Benjamin Guerra Estay 21686594-4 ICCI
+// Joaquin Antonio Berenguela Caimanque 21708988-3  
+
+
+
 package taller3;
 
 import java.util.ArrayList;
@@ -22,3 +27,4 @@ public class PrioridadPorComplejidadStrategy implements PrioridadStrategy{
 		
 	}
 }
+
