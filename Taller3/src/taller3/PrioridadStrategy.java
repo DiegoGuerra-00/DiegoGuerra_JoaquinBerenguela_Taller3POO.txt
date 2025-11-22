3cbf49650af4af6d0417c18d4ed8249a4a03c11e
DiegoGuerra-00 <diego.guerra01@alumnos.ucn.cl>
2025-11-21 22:39:26 -0300

    Update PrioridadStrategy.java

--- Taller3/src/taller3/PrioridadStrategy.java
+++ Taller3/src/taller3/PrioridadStrategy.java
@@ -1,3 +1,6 @@
+// Diego Benjamin Guerra Estay 21686594-4 ICCI
+// Joaquin Antonio Berenguela Caimanque 21708988-3  
+
 package taller3;
 
 import java.util.ArrayList;
@@ -5,3 +8,4 @@ import java.util.ArrayList;
 public interface PrioridadStrategy {	
 	void asignarPrioridad(ArrayList<Tarea> listaTareas);
 }
+
