fdfd5e4ca28bdea22f275a921a5c0fa6ef71c9fd
DiegoGuerra-00 <diego.guerra01@alumnos.ucn.cl>
2025-11-21 22:40:35 -0300

    Update Sistema.java

--- Taller3/src/taller3/Sistema.java
+++ Taller3/src/taller3/Sistema.java
@@ -1,3 +1,8 @@
+// Diego Benjamin Guerra Estay 21686594-4 ICCI
+// Joaquin Antonio Berenguela Caimanque 21708988-3  
+
+
+
 package taller3;
 
 public interface Sistema {
@@ -21,3 +26,4 @@ public interface Sistema {
 
 
 }
+
