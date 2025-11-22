ea6d7dd722059f7cff78b55adae84c84519e8085
DiegoGuerra-00 <diego.guerra01@alumnos.ucn.cl>
2025-11-21 22:42:14 -0300

    Update Visitor.java

--- Taller3/src/taller3/Visitor.java
+++ Taller3/src/taller3/Visitor.java
@@ -1,3 +1,7 @@
+// Diego Benjamin Guerra Estay 21686594-4 ICCI
+// Joaquin Antonio Berenguela Caimanque 21708988-3  
+
+
 package taller3;
 
 import java.util.ArrayList;
@@ -6,3 +10,4 @@ public interface Visitor {
 	void visit(Tarea tarea);
 
 }
+
